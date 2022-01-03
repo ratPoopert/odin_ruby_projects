852ed21ee43aa635f53c8a0ff1a4e90bce78bbff
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2022-01-03 14:04:56 -0500

    Create new variable for hash

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,18 +1,18 @@
 def stock_picker(prices)
 
   # Convert prices array to a hash
-  prices = prices.each_with_index do |price, day|
+  prices_and_days = prices.each_with_index do |price, day|
     prices[day] = {day: day, price: price}
   end
   
   # Set initial variables for comparison
-  buy_price = prices.first[:price]
-  buy_day = prices.first[:day]
-  sell_price = prices.last[:price]
-  sell_day = prices.last[:day]
+  buy_price = prices_and_days.first[:price]
+  buy_day = prices_and_days.first[:day]
+  sell_price = prices_and_days.last[:price]
+  sell_day = prices_and_days.last[:day]
 
   # Find best buy and sale prices. Sell day must be after buy day.
-  prices.each do |this|
+  prices_and_days.each do |this|
     if this[:price] < buy_price && this[:day] < sell_day
       buy_price = this[:price]
       buy_day = this[:day]
